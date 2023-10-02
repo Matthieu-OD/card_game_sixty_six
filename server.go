4b1bd3d8a2ac7e0eb52b90c04f2946fd5d536e39
Matthieu Olenga Disashi <matthieuolengadisashi@Matthieus-Mac-mini-2.local>
2023-10-02 16:33:54 +0200

    Adds echo and start to setup the templates

---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Static("/static", "static")
+	e.Logger.Fatal(e.Start(":1323"))
+}
