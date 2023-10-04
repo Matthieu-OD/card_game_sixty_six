a43c7645bf90df7bf5861b769114eaaac2f99355
Matthieu Olenga Disashi <matthieuolengadisashi@Matthieus-Mac-mini-2.local>
2023-10-04 23:05:46 +0200

    Integrates the home page

--- server/server.go
+++ server/server.go
@@ -23,28 +23,14 @@ func main() {
 	e := echo.New()
 
 	renderer := &TemplateRenderer{
-		templates: template.Must(template.ParseGlob("public/*.html")),
+		templates: template.Must(template.ParseGlob("public/**/*.html")),
 	}
 
 	e.Renderer = renderer
 
-	// viteURL, _ := url.Parse("http://localhost:5173")
-	// proxy := httputil.NewSingleHostReverseProxy(viteURL)
-	//
-	// e.GET("/", func(c echo.Context) error {
-	// 	proxy.ServeHTTP(c.Response(), c.Request())
-	// 	return nil
-	// })
-
-	e.GET("/something", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "template.html", map[string]interface{}{
-			"name": "Dolly!",
-		})
-	}).Name = "foobar"
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "views/home", map[string]interface{}{})
+	})
 
 	e.Logger.Fatal(e.Start(":8000"))
-}
-
-func Hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", "World")
 }
