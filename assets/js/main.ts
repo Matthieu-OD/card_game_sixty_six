212287fdd8924e6f27270a2bd486d0ca6bdfa484
Matthieu-OD <matt.olenga@gmail.com>
2023-10-08 15:26:58 +0200

    Adds htmx and the first call

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -1,8 +1,10 @@
 import 'vite/modulepreload-polyfill'
+import htmx from 'htmx.org'
 
 import '../css/style.css'
 import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
+window.htmx = htmx
 
 Alpine.start()
